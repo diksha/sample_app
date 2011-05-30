c6a2f3a7dbca26541855d3bd9f584c7a2de23d82
Diksha <gr8bigdg@gmail.com>
2011-05-30 19:23:23 +0530

    Added new links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,19 @@
 class PagesController < ApplicationController
   def home
-  @title="diksha"
+  @title="Home"
   end
 
   def contact
+  @title="Contact"
   end
+  
+  def about
+  @title="About"
+  end
+  
+  def help
+@title = "Help"
+end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ SampleApp::Application.routes.draw do
 
   get "pages/contact"
   get "pages/about"
-
+  get "pages/help"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -53,7 +53,12 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => "home#index"
+   match '/', :to => 'pages#home'
+
+   match '/contact', :to => 'pages#contact'
+match '/about',:to => 'pages#about'
+match '/help',:to => 'pages#help'
+
 
   # See how all your routes lay out with "rake routes"
 
